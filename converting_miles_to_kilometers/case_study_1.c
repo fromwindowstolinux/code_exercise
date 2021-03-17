819ce18a70c9913bcae1bc893259bb9e05934172
fromwindowstolinux <nhiroin@gmail.com>
2021-03-17 10:25:07 +0800

    add c file

--- converting_miles_to_kilometers/case_study_1.c
+++ converting_miles_to_kilometers/case_study_1.c
@@ -1,1 +1,19 @@
+// Converts distance in miles to kilometers
 
+#include <stdio.h>
+#define KILOMETER_PER_MILE 1.609
+
+int main(void)
+{
+    double miles = 0;
+    double kilometers = 0;
+
+    printf("Enter the distance in miles : ");
+    scanf("%lf", &miles);
+
+    kilometers = KILOMETER_PER_MILE * miles;
+
+    printf("%.3lf miles equals to %.3lf kilometers.\n", miles, kilometers);
+
+    return(0);
+}
