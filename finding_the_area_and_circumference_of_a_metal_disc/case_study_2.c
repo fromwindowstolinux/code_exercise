7a8647adb9efa591d3f13c468152b84cbe605156
fromwindowstolinux <nhiroin@gmail.com>
2021-03-17 21:04:33 +0800

    updated c file

--- finding_the_area_and_circumference_of_a_metal_disc/case_study_2.c
+++ finding_the_area_and_circumference_of_a_metal_disc/case_study_2.c
@@ -1,1 +1,38 @@
+/*
+Calculates and displays the area and circumference of a metal disc given in the radius in centimeters
+*/
 
+#include <stdio.h>
+#define PI 3.14159
+
+int main (void) {
+
+    char part_id1, part_id2, part_id3;
+    // input 3 character part id
+    double radius;
+    // input radius of disc in cm
+    double area;
+    // output - area of disc in cm^2
+    double circumference;
+    // output - circumference of disc
+
+    printf("Enter a 3-character part ID: ");
+    scanf("%c%c%c", &part_id1, &part_id2, &part_id3);
+    // get the part's ID
+
+    printf("Enter radius in centimeter: ");
+    scanf("%lf", &radius);
+    // get the disc radius 
+
+    area = PI * radius * radius;
+    // calculate the area
+
+    circumference = 2 * PI * radius;
+    // calculate the circumference
+
+    printf("For part %c%c%c: \n", part_id1, part_id2, part_id3);
+    printf("The disc area is %f cm^2.\n", area);
+    printf("The circumference is %f cm.\n", circumference);
+
+    return 0;
+}
