8cba8e268456b69416b8611d504065d518d9d80d
fromwindowstolinux <nhiroin@gmail.com>
2021-03-17 11:27:20 +0800

    completed files

--- converting_miles_to_kilometers/case_study_1.java
+++ converting_miles_to_kilometers/case_study_1.java
@@ -1,14 +1,19 @@
-//CODES ON CLIFF HANGER//
-
 // Converts distance in miles to kilometers
 
 import java.util.Scanner;
 
-class MyClass 
-{
-    public static void main(Int[ ] args)
-    {
-        Scanner miles = new Scanner(System.in);
-        System.out.println(miles.nextInt());        
+class case_study_1 {
+
+    public static void main(String []args) {
+
+        double miles=0;
+        double kilometers=0;
+        double kilometers_per_miles = 1.609;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the distance in miles : ");
+        miles = scan.nextInt();
+        kilometers = kilometers_per_miles * miles;
+        System.out.println(miles+" miles equals to "+kilometers+" kilometers.");
     }
 }
