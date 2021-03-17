e7fcb918aca5c09e3fd8d41ec760477735a49d49
fromwindowstolinux <43229477+fromwindowstolinux@users.noreply.github.com>
2021-03-17 10:06:45 +0800

    Delete problem_solving directory

--- problem_solving/problem_solving.py
+++
@@ -1,6 +0,0 @@
-# Converts distance in miles to kilometers
-
-miles = input("Enter distance in miles : ")
-kilometers_per_miles = 1.609
-kilometer = kilometers_per_miles * float(miles)
-print(f"{miles} miles equals to {kilometer} kilometers.")
